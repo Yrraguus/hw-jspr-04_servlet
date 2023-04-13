b902246d862c413c6ec93fdde6c7f3126ba65c24
Igor Yelshin <garry.by.ru@bk.ru>
2023-04-13 18:25:08 +0300

    added files

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,47 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class PostRepository {
+    AtomicLong counter = new AtomicLong();
+    public static final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+
+    public ConcurrentHashMap<Long, Post> all() {
+        if (posts.isEmpty()) {
+            throw new NotFoundException("Empty list");
+        }
+        return posts;
+    }
+
+    public Optional<Post> getById(long id) {
+        if (posts.containsKey(id)) {
+            return Optional.ofNullable(posts.get(id));
+        }
+        throw new NotFoundException("Post not found");
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            posts.put(counter.incrementAndGet(), post);
+            return post;
+        } else {
+            if (!posts.containsKey(post.getId())) {
+                throw new NotFoundException("Post");
+            } else {
+                posts.put(post.getId(), post);
+            }
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (posts.containsKey(id)) {
+            posts.remove(id);
+        } else throw new NotFoundException("Post doesn't exist");
+    }
+}
---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,57 @@
+package ru.netology.servlet;
+
+import ru.netology.controller.PostController;
+import ru.netology.exception.NotFoundException;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+    private final String GET = "GET";
+    private final String POST = "POST";
+    private final String DELETE = "DELETE";
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            if (method.equals(GET)) {
+                if (path.equals("/api/posts")) {
+                    controller.all(resp);
+                } else if (path.matches("/api/posts/\\d+")) {
+                    final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+                    controller.getById(id, resp);
+                }
+                return;
+            }
+            if (method.equals(POST) && path.equals("/api/posts")) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(DELETE) && path.matches("/api/posts/\\d+")) {
+                final var id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (NotFoundException e) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
